3532ab77d275a3ad544c7730a8973721815c5719
mauritoharretche <mauroharretche@gmail.com>
2023-02-25 18:55:49 -0300

    operadores logicos

---
+++ condionales/if_else.py
@@ -0,0 +1,9 @@
+contraseña_almacenada = "MauritoHarretche"
+contraseña_escrita = "MauritoHarretche"
+
+if contraseña_almacenada == contraseña_escrita:
+    print("INICIANDO SESION...")
+else:
+    print("contraseña equivocada, intente de nuevo")
+
+
