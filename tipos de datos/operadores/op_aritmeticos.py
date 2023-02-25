806b8105eb774173c9fdc6dc1a0b61800355aa79
mauritoharretche <mauroharretche@gmail.com>
2023-02-25 17:32:33 -0300

    operadores comparacion

---
+++ tipos de datos/operadores/op_aritmeticos.py
@@ -0,0 +1,20 @@
+#suma y resta ( + y -)
+suma = 12 + 5
+resta = 12 - 5
+
+#multi y división ( * y / )
+multi = 12 * 5
+división = 12 / 5
+
+#potenciación (exponente)
+exponente = 12 ** 5
+
+#división baja 
+division_baja = 12 // 5  #devuelve entero redondeado hacia abajo
+
+#resto o módulo
+resto = 12 % 5
+
+tipo_de_dato = type(division_baja) #type nos devuelve el tipo de dato que es
+
+print(tipo_de_dato)
